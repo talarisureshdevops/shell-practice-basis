6a0fecd2b08bea67c329bcae078a694dafed4fe6
Talari <suresh.talari-ext@hpe.com>
2026-05-24 10:45:04 +0530

    added new

---
+++ 02-Conversion.sh
@@ -0,0 +1,7 @@
+!/bin/bash
+
+echo "suresh:: Hello ramehs, how are you?"
+echo "ramesh:: Hi suresh, I am fine thanks for asking, how are you doing fine?"
+echo "suresh:: doing fine, what about you are learning ramesh"
+echo "ramesh:: I am learning shell, what about you?"
+
