02f7e7aa1ec4a7f8ecd8249af5c40bbba0f8d5b1
Talari <suresh.talari-ext@hpe.com>
2026-05-24 12:07:51 +0530

    added new one

---
+++ 04-Variables.sh
@@ -0,0 +1,9 @@
+!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1:: Hello $PERSON2, how are you?"
+echo "$PERSON2:: Hi $PERSON1, I am fine thanks for asking, how are you doing fine?"
+echo "$PERSON1:: doing fine, what about you are learning $PERSON2"
+echo "$PERSON2:: I am learning shell, what about you?"
