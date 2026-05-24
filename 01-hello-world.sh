dbea95e869c7fe4ec54158334ea611fb5e45e56c
Talari <suresh.talari-ext@hpe.com>
2026-05-24 10:26:50 +0530

    added new one

---
+++ 01-hello-world.sh
@@ -0,0 +1,10 @@
+!/bin/bash
+# ============================================
+# Script: 01-hello-world.sh
+# Purpose: print the hello world program
+# ============================================
+
+echo "Hello world"
+echo "Hi, I am learning git from internet"
+
+
