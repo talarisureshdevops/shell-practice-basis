f83d24f6dbe04ab93302161395e1af53a46ca2ed
Talari <suresh.talari-ext@hpe.com>
2026-05-24 12:16:07 +0530

    added new file

---
+++ 05-Variables.sh
@@ -0,0 +1,11 @@
+!/bin/bash
+
+echo "please enter your name:"
+read USER_NAME
+echo "user name is $USER_NAME"
+
+echo "please enter your password::"
+read -s PASSWORD
+
+echo "Password is $PASSWORD"
+
