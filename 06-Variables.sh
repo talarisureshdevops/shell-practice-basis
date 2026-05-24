c8ef2bda52eb5a5202362e83ac99ab6763deeb5d
Talari <suresh.talari-ext@hpe.com>
2026-05-24 12:30:50 +0530

    added

--- 06-Variables.sh
+++ 06-Variables.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-Start_time=$(date +%s)
-echo "Script Executed at :$start_time"
+start_time=$(date +%s)
+echo "Script Executed at : $start_time"
 
 sleep 10
 
 end_time=$(date +%s)
-total_time=$(($end_time-$start_time))
+total_time=$(($end_time - $start_time))
 
 echo "The script Executed in : $total_time seconds"
