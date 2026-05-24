12073b8bfa811fe57d5b492e8f4c963af7d0d656
Talari <suresh.talari-ext@hpe.com>
2026-05-24 14:50:30 +0530

    added new

---
+++ 09-conditions.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+NUMBER=$1
+
+if [ $NUMBER -gt 20]; then
+  echo " the given number: $NUMBER is greater than 20"
+elif [ $NUMBER -eq 20]; then 
+  echo " the given number: $NUMBER is equal to 20"
+else
+  echo " the given number: $NUMBER is less than 20"
+fi
+
