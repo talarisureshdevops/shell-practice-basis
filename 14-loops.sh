fd976bbd051755013b1be7e2f68b9559f60a08a5
Talari <suresh.talari-ext@hpe.com>
2026-05-25 11:23:58 +0530

    added new

---
+++ 14-loops.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+LOGS_FOLDER="/var/log/shell-script"
+LOGS_FILE="/var/log/shell-script/$0.log"
+
+if [ $USERID -ne 0 ]; then
+   echo "please run this script with root user access" | tee -e $LOGS_FILE
+   exit 1
+fi
+mkdir -p $LOGS_FOLDER
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo "$2 ... FAILURE" | tee -a $LOGS_FILE
+        exit 1
+    else
+        echo "$2 ... SUCCESS" | tee -a $LOGS_FILE
+    fi
+}
+
+for package in $@ #mysql, nodejs,nginx
+do
+   dnf install $package -y &>>$LOGS_FILE
+   validate $?  "$package installtion"
+done
+
